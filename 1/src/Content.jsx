a90274b35c445f76d6e19aae8a3511f20c1fd4e1
Selahattin ÇİNÇİN <selocincin@hotmail.com>
2017-10-23 20:28:05 +0300

    Default React Project

---
+++ 1/src/Content.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Content extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Sayfaaaa</h2>
+      </div>
+    );
+  }
+}
+export default Content;
---
+++ gamedeneme/src/Content.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Content extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Content Ben</h2>
+      </div>
+    );
+
+  }
+}
+export default Content;
